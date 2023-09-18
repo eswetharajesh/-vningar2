ca7405e9390315ac5dbb1210e128ee6c471afa53
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2023-09-18 15:05:17 +0200

    Formatted code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 bool isOpen = true;
 
-while(isOpen){
+while (isOpen)
+{
     Console.WriteLine("Main Menu :");
     Console.WriteLine("1. Youth Or Pensioner");
     Console.WriteLine("2. Calculate Total Price");
@@ -11,111 +12,119 @@ while(isOpen){
     Console.WriteLine("Enter your choice: ");
     string choice = Console.ReadLine();
 
-    switch(choice)
+    switch (choice)
     {
         case "0":
             {
-            isOpen = false;
-            break;
-        }
+                isOpen = false;
+                break;
+            }
         case "1":
-        {
-            checkAge();
-            break;
-        }
+            {
+                checkAge();
+                break;
+            }
         case "2":
-        {
-            calculateTotalCost();
-            break;
-        }
+            {
+                calculateTotalCost();
+                break;
+            }
         case "3":
-        {
-            RepeatTenTimes();
-            break;
-        }
+            {
+                RepeatTenTimes();
+                break;
+            }
         case "4":
-        {
-            GetThirdWord();
-            break;
-        }
+            {
+                GetThirdWord();
+                break;
+            }
         default:
-        Console.WriteLine("Invalid choice.Please try again.");
-        break;
+            Console.WriteLine("Invalid choice.Please try again.");
+            break;
 
     }
 }
 
 /*TO check the category of person by age*/
 void checkAge()
- {
-Console.WriteLine("Enter the age: ");
-if(int.TryParse(Console.ReadLine(), out int age))
 {
-    if(age > 0){
-        if((age < 5) || (age > 100))
+    Console.WriteLine("Enter the age: ");
+    if (int.TryParse(Console.ReadLine(), out int age))
     {
-        Console.WriteLine("Free");
-        }
-        else if(age < 20)
+        if (age > 0)
         {
-            Console.WriteLine("Youth's Price: SEK 80");
+            if ((age < 5) || (age > 100))
+            {
+                Console.WriteLine("Free");
+            }
+            else if (age < 20)
+            {
+                Console.WriteLine("Youth's Price: SEK 80");
+            }
+            else if (age > 64)
+            {
+                Console.WriteLine("Pensioner's Price: SEK 90");
+            }
+            else
+            {
+                Console.WriteLine("Standard Price: SEK 120");
+            }
         }
-        else if(age > 64)
+        else
         {
-            Console.WriteLine("Pensioner's Price: SEK 90");
+            Console.WriteLine("Age should be greater than Zero!");
         }
-        else{
-            Console.WriteLine("Standard Price: SEK 120");
-       }
-    }else{
-        Console.WriteLine("Age should be greater than Zero!");
-    } 
-}
-else{
-    Console.WriteLine("Please Enter a valid age.");
-}
+    }
+    else
+    {
+        Console.WriteLine("Please Enter a valid age.");
+    }
 }
 
 
 /*To calculate total amount and count of people*/
 void calculateTotalCost()
 {
-Console.WriteLine("Enter total number of people");
-if(int.TryParse(Console.ReadLine(), out int peopleCount))
-{
-    int totalCost = 0;
-    for(int i = 0; i < peopleCount; i++)
+    Console.WriteLine("Enter total number of people");
+    if (int.TryParse(Console.ReadLine(), out int peopleCount))
     {
-Console.WriteLine($"Enter age for person {i + 1}:  ");
-if(int.TryParse(Console.ReadLine(), out int age))
-{
-    if(age < 5  || age > 100) // children under 5 and senior above 100 go for free
+        int totalCost = 0;
+        for (int i = 0; i < peopleCount; i++)
+        {
+            Console.WriteLine($"Enter age for person {i + 1}:  ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                if (age < 5 || age > 100) // children under 5 and senior above 100 go for free
+                {
+                    continue;
+                }
+                else if (age < 20)
+                {
+                    totalCost += 80;
+                }
+                else if (age > 64)
+                {
+                    totalCost += 90;
+                }
+                else
+                {
+                    totalCost += 120;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid age. Please enter again.");
+
+            }
+        }
+        Console.WriteLine($"Number Of People: {peopleCount}");
+        Console.WriteLine($"Total Amount: {totalCost}");
+    }
+    else
     {
-        continue;
+        Console.WriteLine("Please enter a valid input.");
     }
-    else if( age < 20)
-    {
-        totalCost += 80 ;
-    }
-    else if(age > 64)
-    {
-        totalCost += 90;
-    }
-    else{
-        totalCost += 120;
-    }
-}
-else{
-    Console.WriteLine("Invalid age. Please enter again.");
-    
-}
-    }
-    Console.WriteLine($"Number Of People: {peopleCount}");
-    Console.WriteLine($"Total Amount: {totalCost}");
-}
-else{
-    Console.WriteLine("Please enter a valid input.");
-}
 }
 
 
@@ -124,28 +133,29 @@ void RepeatTenTimes()
 {
     Console.WriteLine("Enter a text: ");
     string inputText = Console.ReadLine();
-    for(int i = 0; i <= 10; i++)
+    for (int i = 0; i <= 10; i++)
     {
-Console.Write($"{i}. {inputText} ");
+        Console.Write($"{i}. {inputText} ");
     }
-Console.WriteLine();
+    Console.WriteLine();
 }
 
 
 /*To print the third word*/
 void GetThirdWord()
 {
-Console.WriteLine("Enter a sentence with atleast 3 words: ");
-string text = Console.ReadLine();
-string[] words = System.Text.RegularExpressions.Regex.Split(text, @"\s+");
+    Console.WriteLine("Enter a sentence with atleast 3 words: ");
+    string text = Console.ReadLine();
+    string[] words = System.Text.RegularExpressions.Regex.Split(text, @"\s+");
 
-if (words.Length >= 3)
-{
-    string thirdword = words[2];
-    Console.WriteLine($"The Third Word is: {thirdword}");
-}
-else{
-    Console.WriteLine("Please enter valid text. ");
-}
+    if (words.Length >= 3)
+    {
+        string thirdword = words[2];
+        Console.WriteLine($"The Third Word is: {thirdword}");
+    }
+    else
+    {
+        Console.WriteLine("Please enter valid text. ");
+    }
 }
 
